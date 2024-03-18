2d30b117f52d5ddc5a77d0a1dc88d0c2aaaf2250
beMimg <bernardommguerreiro@gmail.com>
2024-03-18 16:00:27 +0100

    Add routesForPublic to ReactProvider

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,18 +1,26 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
-// Unauthanticated Elements:
+// Unauthanticated Routes:
 import UnauthenticatedHomepage from "./page/UnauthenticatedHomepage";
 import SignUp from "./page/SignUp";
 import LogIn from "./page/LogIn";
 
+// Public Routes:
+import AboutUs from "./page/AboutUs";
+
 const Routes = () => {
+  const routesForPublic = [{ path: "/about-us", element: <AboutUs /> }];
+
   const routesForUnauthenticatedOnly = [
     { path: "/", element: <UnauthenticatedHomepage /> },
     { path: "/sign-up", element: <SignUp /> },
     { path: "/login", element: <LogIn /> },
   ];
 
-  const router = createBrowserRouter([...routesForUnauthenticatedOnly]);
+  const router = createBrowserRouter([
+    ...routesForPublic,
+    ...routesForUnauthenticatedOnly,
+  ]);
 
   return <RouterProvider router={router}></RouterProvider>;
 };
