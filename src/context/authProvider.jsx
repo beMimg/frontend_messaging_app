7c1f34de3b4f9fc417de0fc6b80e65aaa19ecc9b
beMimg <bernardommguerreiro@gmail.com>
2024-03-18 17:42:31 +0100

    Add authProvider

---
+++ src/context/authProvider.jsx
@@ -0,0 +1,36 @@
+import axios from "axios";
+import { useContext } from "react";
+import { createContext, useEffect, useState } from "react";
+
+const AuthContext = createContext();
+
+function AuthProvider({ children }) {
+  const [token, setToken] = useState(localStorage.getItem("token"));
+
+  useEffect(() => {
+    if (token) {
+      axios.defaults.headers.common["Authorization"] = "Bearer " + token;
+      localStorage.setItem("token", token);
+    } else {
+      delete axios.defaults.headers.common["Authorization"];
+      localStorage.removeItem("token");
+    }
+  }, [token]);
+
+  const contextValue = useMemo(() => {
+    return {
+      token,
+      setToken,
+    };
+  }, [token]);
+
+  return (
+    <AuthContext.Provider value={contextValue}>{children}</AuthContext.Provider>
+  );
+}
+
+export function useAuth() {
+  return useContext(AuthContext);
+}
+
+export default AuthProvider;
