39b09510d6b50357d8e174be940d93e5a7e4a570
beMimg <bernardommguerreiro@gmail.com>
2024-03-18 18:34:52 +0100

    Add UnauthenticatedHome layout and style

--- src/page/UnauthenticatedPages/UnauthenticatedHomepage.jsx
+++ src/page/UnauthenticatedPages/UnauthenticatedHomepage.jsx
@@ -1,7 +1,57 @@
+import { Link } from "react-router-dom";
+import glassesKissSvg from "../../assets/reshot-icon-glasses-kiss-YUSND43AHW.svg";
+import demonSvg from "../../assets/reshot-icon-demon-RHZBQ8E7SN.svg";
+import loveSvg from "../../assets/reshot-icon-love-U63YHWCBJX.svg";
+import moneySvg from "../../assets/reshot-icon-money-S6DHLMN347.svg";
+import tongeSvg from "../../assets/reshot-icon-tongue-Y92VG4RXPL.svg";
+
 export default function UnauthenticatedHomepage() {
   return (
-    <div>
-      <p>lol</p>
+    <div className="grid h-screen grid-rows-2 bg-gradient-to-b from-teal-600 to-teal-900 p-6 text-white">
+      <div className="relative">
+        <img
+          src={glassesKissSvg}
+          alt="glasses kiss emoji "
+          className=" absolute h-40 rotate-12"
+        />
+        <img
+          src={demonSvg}
+          alt="demon emoji"
+          className=" absolute bottom-0 right-0 h-24 rotate-[30deg] "
+        />
+        <img
+          src={loveSvg}
+          alt="love moji"
+          className="absolute bottom-12 left-6 h-16 -rotate-12"
+        />
+        <img
+          src={moneySvg}
+          alt="money face emoji"
+          className="absolute right-0 h-16  rotate-12"
+        />
+        <img
+          src={tongeSvg}
+          alt="tonge out emoji"
+          className="absolute bottom-32 right-[78px] h-16"
+        />
+      </div>
+      <div className="flex flex-col justify-evenly">
+        <div className="flex flex-col gap-4">
+          <h1 className="text-5xl">Let's Get Started</h1>
+          <p>
+            Connect with each other with catting. Enjoy safe and private texting
+          </p>
+        </div>
+        <button className="w-full rounded-lg bg-white p-2 text-center font-semibold text-teal-600">
+          Start Chatting
+        </button>
+        <p>
+          Already have an account?{" "}
+          <Link to="/login" className=" font-bold">
+            Login
+          </Link>
+        </p>
+      </div>
     </div>
   );
 }
