c245a9b1a41821ab665bce54372e12f811f91ed7
beMimg <bernardommguerreiro@gmail.com>
2024-03-20 19:44:32 +0100

    Add Link To Followings to User VisitedProfile

--- src/components/Followings.jsx
+++ src/components/Followings.jsx
@@ -30,18 +30,15 @@ export default function Followings() {
 
   return (
     followings && (
-      <div>
-        <div className="flex flex-row gap-3">
-          <h1 className="text-md font-semibold">Following</h1>
-          <p>{followings.length}</p>
+      <div className="flex flex-col gap-3">
+        <div className="flex flex-row gap-2">
+          <h1 className="text-md items-center font-semibold">Following</h1>
+          <p className="text-center">{followings.length}</p>
         </div>
-        <div className=" max-h-[300px]">
+        <div className=" h-[200px]">
           {followings.map((follow) => (
-            <Link>
-              <div
-                key={follow._id}
-                className="h-[60px] w-[60px] rounded-full bg-teal-600"
-              ></div>
+            <Link to={`/users/${follow._id}`} key={follow._id}>
+              <div className="h-[60px] w-[60px] rounded-full bg-teal-600"></div>
             </Link>
           ))}
         </div>
