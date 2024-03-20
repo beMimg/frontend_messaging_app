89d3ffebac9bf457b53738bd38b043fce20d9c1f
beMimg <bernardommguerreiro@gmail.com>
2024-03-20 20:13:29 +0100

    Nav Links to / and /profile

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -6,13 +6,13 @@ import { Link } from "react-router-dom";
 export default function Nav() {
   return (
     <nav className="absolute bottom-5 left-[100px] flex  flex-row gap-6 rounded-lg text-center">
-      <Link className="p-2 text-2xl">
+      <Link to="/" className="p-2 text-2xl">
         <FiMessageSquare />
       </Link>
       <Link className="p-2 text-2xl">
         <FiSearch />
       </Link>
-      <Link className="p-2 text-2xl">
+      <Link to="/profile" className="p-2 text-2xl">
         <IoPersonSharp />
       </Link>
     </nav>
