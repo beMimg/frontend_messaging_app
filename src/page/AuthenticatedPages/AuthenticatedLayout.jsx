46b54ca1460ae28c90939c90d8cb1e54cafb5e54
beMimg <bernardommguerreiro@gmail.com>
2024-03-24 10:59:55 +0100

    Layout UI Changes for large devices

--- src/page/AuthenticatedPages/AuthenticatedLayout.jsx
+++ src/page/AuthenticatedPages/AuthenticatedLayout.jsx
@@ -3,11 +3,11 @@ import Nav from "../../components/Nav";
 
 export default function AuthenticatedLayout() {
   return (
-    <div className="lg:flex lg:flex-row">
-      <div className="absolute bottom-5  flex w-full items-center justify-center lg:static lg:h-screen lg:w-[100px] ">
+    <div className="dark:bg-neutral-800 dark:text-white lg:flex lg:h-screen lg:flex-row lg:gap-2 lg:overflow-hidden lg:p-3 ">
+      <div className="absolute bottom-5 flex w-full items-center justify-center bg-gray-200  lg:static lg:w-[100px]  lg:rounded-2xl lg:dark:bg-neutral-950">
         <Nav></Nav>
       </div>
-      <div className="lg:flex-1">
+      <div className="bg-gray-200 lg:flex-1 lg:rounded-lg lg:dark:bg-neutral-950">
         <Outlet />
       </div>
     </div>
