692ee0656c843f17a46afb1a683be2553b3e8ece
beMimg <bernardommguerreiro@gmail.com>
2024-03-24 09:27:33 +0100

    profile pic to MessagesPageHeader

--- src/components/MessagesPageHeader.jsx
+++ src/components/MessagesPageHeader.jsx
@@ -1,13 +1,26 @@
 import { useAuth } from "../context/authProvider";
 import { SlOptionsVertical } from "react-icons/sl";
+import glassesKissSvg from "../assets/reshot-icon-glasses-kiss-YUSND43AHW.svg";
 
 export default function MessagesPageHeader() {
   const { user } = useAuth();
 
+  console.log(user);
   return (
     <div className="flex flex-row items-center justify-between">
       <div className="flex flex-row items-center gap-2">
-        <div className="h-[60px] w-[60px] rounded-full bg-teal-600"></div>
+        {user.profile_pic_src ? (
+          <img
+            className="h-[60px] w-[60px] rounded-full border border-gray-300  object-cover object-center"
+            src={user.profile_pic_src}
+            alt=""
+          />
+        ) : (
+          <img
+            src={glassesKissSvg}
+            className="h-[60px] w-[60px] rounded-full border border-gray-300  object-cover object-center"
+          />
+        )}
         <div className="flex flex-col">
           <h1 className="pr-2 font-semibold">{user.first_name} </h1>
           <p>@{user.username}</p>
