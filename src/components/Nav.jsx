f59983191c0e358b0bdaeabfaacaff8924ded2fb
beMimg <bernardommguerreiro@gmail.com>
2024-03-24 10:36:05 +0100

    responsive nav, on 1024px nav is on the left

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,18 +5,16 @@ import { Link } from "react-router-dom";
 
 export default function Nav() {
   return (
-    <div className="absolute bottom-5  flex w-full items-center justify-center">
-      <nav className=" flex  flex-row gap-6 rounded-lg text-center">
-        <Link to="/" className="p-2 text-2xl">
-          <FiMessageSquare />
-        </Link>
-        <Link className="p-2 text-2xl">
-          <FiSearch />
-        </Link>
-        <Link to="/profile" className="p-2 text-2xl">
-          <GoPerson />
-        </Link>
-      </nav>
-    </div>
+    <nav className=" flex  flex-row gap-6 rounded-lg text-center lg:flex-col lg:items-center lg:justify-center">
+      <Link to="/" className="p-2 text-2xl">
+        <FiMessageSquare />
+      </Link>
+      <Link className="p-2 text-2xl">
+        <FiSearch />
+      </Link>
+      <Link to="/profile" className="p-2 text-2xl">
+        <GoPerson />
+      </Link>
+    </nav>
   );
 }
--- src/page/AuthenticatedPages/AuthenticatedLayout.jsx
+++ src/page/AuthenticatedPages/AuthenticatedLayout.jsx
@@ -3,9 +3,13 @@ import Nav from "../../components/Nav";
 
 export default function AuthenticatedLayout() {
   return (
-    <div>
-      <Outlet />
-      <Nav></Nav>
+    <div className="lg:flex lg:flex-row">
+      <div className="absolute bottom-5  flex w-full items-center justify-center lg:static lg:h-screen lg:w-[100px] ">
+        <Nav></Nav>
+      </div>
+      <div className="lg:flex-1">
+        <Outlet />
+      </div>
     </div>
   );
 }
