1694660c44c165d74f10731ff7fd5c5947227df6
beMimg <bernardommguerreiro@gmail.com>
2024-03-24 11:09:45 +0100

    Add borders to ConversationPage

--- src/page/AuthenticatedPages/ConversationPage.jsx
+++ src/page/AuthenticatedPages/ConversationPage.jsx
@@ -5,14 +5,14 @@ export default function ConversationPage() {
   const { id } = useParams();
 
   return (
-    <div className="lg:grid lg:h-screen lg:grid-cols-5 xl:grid-cols-6">
-      <div className="hidden lg:col-span-2 lg:grid">
+    <div className=" lg:grid lg:h-full lg:grid-cols-5 xl:grid-cols-6">
+      <div className="hidden border-r border-white dark:border-neutral-800 lg:col-span-2  lg:grid ">
         <MessagesPage />
       </div>
-      <div className="bg-red-600 lg:col-span-3">
+      <div className=" border-r border-white p-2 dark:border-neutral-800 lg:col-span-3">
         <h1>conversation {id}</h1>
       </div>
-      <div className="hidden bg-green-600  xl:grid">
+      <div className="hidden xl:grid xl:p-2">
         <p>lol</p>
       </div>
     </div>
