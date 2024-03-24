aa816c163ff0fce990c0a5b3761b8e5770298d23
beMimg <bernardommguerreiro@gmail.com>
2024-03-24 10:20:31 +0100

    modify nav to be always centered no matter device

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,16 +5,18 @@ import { Link } from "react-router-dom";
 
 export default function Nav() {
   return (
-    <nav className="absolute bottom-5 left-[100px] flex  flex-row gap-6 rounded-lg text-center">
-      <Link to="/" className="p-2 text-2xl">
-        <FiMessageSquare />
-      </Link>
-      <Link className="p-2 text-2xl">
-        <FiSearch />
-      </Link>
-      <Link to="/profile" className="p-2 text-2xl">
-        <GoPerson />
-      </Link>
-    </nav>
+    <div className="absolute bottom-5  flex w-full items-center justify-center">
+      <nav className=" flex  flex-row gap-6 rounded-lg text-center">
+        <Link to="/" className="p-2 text-2xl">
+          <FiMessageSquare />
+        </Link>
+        <Link className="p-2 text-2xl">
+          <FiSearch />
+        </Link>
+        <Link to="/profile" className="p-2 text-2xl">
+          <GoPerson />
+        </Link>
+      </nav>
+    </div>
   );
 }
