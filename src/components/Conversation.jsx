72291f7f5d899377feccd50007d2264ad2a865d8
beMimg <bernardommguerreiro@gmail.com>
2024-03-25 11:51:09 +0100

    messages scroll start at the bottom

--- src/components/Conversation.jsx
+++ src/components/Conversation.jsx
@@ -1,7 +1,7 @@
 import ConversationForm from "./ConversationForm";
 import ConversationHeader from "./ConversationHeader";
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { API_DOMAIN } from "../utils/API_DOMAIN";
 import Messages from "./Messages";
 
@@ -10,6 +10,7 @@ export default function Conversation({ conversation_id }) {
   const [errors, setErrors] = useState();
   const [isLoading, setIsLoading] = useState();
 
+  const messageContainerRef = useRef(null);
   // rerender everytime the conversation_id changes and fetch that
   useEffect(() => {
     const getConversation = async () => {
@@ -29,12 +30,25 @@ export default function Conversation({ conversation_id }) {
     getConversation();
   }, [conversation_id]);
 
+  // Scroll to the bottom of the page.
+  // Need to set conversationDetails as dependecies because,
+  // There's no height without those conversationDetails.
+  useEffect(() => {
+    if (messageContainerRef.current) {
+      messageContainerRef.current.scrollTop =
+        messageContainerRef.current.scrollHeight;
+    }
+  }, [conversationDetails]);
+
   return (
     <div className="flex h-[88%] flex-col lg:h-full">
       <header>
         <ConversationHeader conversationDetails={conversationDetails} />
       </header>
-      <div className="flex-1 overflow-scroll p-2 dark:bg-neutral-900 ">
+      <div
+        className="flex-1 overflow-auto p-2 dark:bg-neutral-900"
+        ref={messageContainerRef}
+      >
         <Messages
           conversation_id={conversation_id}
           conversationDetails={conversationDetails}
