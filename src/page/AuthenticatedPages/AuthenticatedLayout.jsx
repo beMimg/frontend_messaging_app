62ae6b15fbd34e31b4b580c7a6e9937713eb53f5
beMimg <bernardommguerreiro@gmail.com>
2024-03-25 11:27:27 +0100

    fixed overflow in message

--- src/page/AuthenticatedPages/AuthenticatedLayout.jsx
+++ src/page/AuthenticatedPages/AuthenticatedLayout.jsx
@@ -3,11 +3,11 @@ import Nav from "../../components/Nav";
 
 export default function AuthenticatedLayout() {
   return (
-    <div className="dark:bg-neutral-950 dark:text-white lg:flex lg:h-screen lg:flex-row lg:gap-2 lg:overflow-hidden lg:p-3 lg:dark:bg-neutral-800 ">
+    <div className="dark:bg-neutral-950 dark:text-white lg:flex lg:h-screen lg:flex-row lg:gap-2  lg:p-3 lg:dark:bg-neutral-800 ">
       <div className="absolute bottom-5 flex w-full items-center justify-center   lg:static lg:w-[100px]  lg:rounded-2xl lg:bg-gray-200 lg:dark:bg-neutral-950">
         <Nav></Nav>
       </div>
-      <div className="min-h-screen lg:min-h-0 lg:flex-1 lg:rounded-lg lg:bg-gray-200 lg:dark:bg-neutral-950">
+      <div className=" min-h-screen lg:flex lg:min-h-0 lg:flex-1 lg:rounded-lg lg:bg-gray-200 lg:dark:bg-neutral-950">
         <Outlet />
       </div>
     </div>
--- src/page/AuthenticatedPages/ConversationPage.jsx
+++ src/page/AuthenticatedPages/ConversationPage.jsx
@@ -8,14 +8,15 @@ export default function ConversationPage() {
   const { conversation_id } = useParams();
 
   return (
-    <div className=" lg:grid lg:h-full lg:grid-cols-5 xl:grid-cols-6">
-      <div className="hidden border-r border-white dark:border-neutral-800 lg:col-span-2  lg:grid ">
+    // <div className=" lg:grid lg:h-full lg:grid-cols-5 xl:grid-cols-6">
+    <div className="lg:flex lg:max-h-[100%] lg:w-full lg:flex-row lg:overflow-hidden">
+      <div className="hidden border-r border-white dark:border-neutral-800 lg:flex lg:w-2/6">
         <MessagesPage />
       </div>
-      <div className="h-screen border-r border-white dark:border-neutral-800  lg:col-span-3 lg:h-full">
+      <div className="h-screen border-r border-white dark:border-neutral-800  lg:col-span-3 lg:max-h-full lg:flex-1 lg:overflow-hidden ">
         <Conversation conversation_id={conversation_id} />
       </div>
-      <div className="hidden xl:grid xl:p-2">
+      <div className="hidden lg:flex lg:w-1/6">
         <ConversationDetails />
       </div>
     </div>
