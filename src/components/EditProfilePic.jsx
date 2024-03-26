b705faf6e24f2abdfd50c6841370c656758a1977
beMimg <bernardommguerreiro@gmail.com>
2024-03-26 14:41:28 +0100

    Add functionality to update user picture

--- src/components/EditProfilePic.jsx
+++ src/components/EditProfilePic.jsx
@@ -1,11 +1,67 @@
+import axios from "axios";
+import { useState } from "react";
 import { IoMdClose } from "react-icons/io";
+import { API_DOMAIN } from "../utils/API_DOMAIN";
 
 export default function EditProfilePic({ setIsProfilePicOpen }) {
+  const [img, setImg] = useState();
+  const [errors, setErrors] = useState();
+  const [success, setIsSucess] = useState(false);
+  const [isLoading, setIsLoading] = useState();
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    try {
+      setIsLoading(true);
+      const formData = new FormData();
+      formData.append("img", img);
+
+      const response = await axios.put(
+        `${API_DOMAIN}/users/profile_pic`,
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        },
+      );
+      setIsLoading(false);
+      setIsSucess(response.data.message);
+      setTimeout(() => {
+        window.location.reload();
+      }, [1000]);
+      return;
+    } catch (err) {
+      return setErrors(
+        "Could not update your profile picture, please try again later.",
+      );
+    }
+  }
   return (
     <div className="absolute left-0 top-0 z-20 flex h-full w-full items-center justify-center bg-black bg-opacity-90 ">
-      <form className="relative flex flex-col gap-4 rounded-lg bg-white  p-6 dark:bg-neutral-900 dark:text-white">
-        <input type="file" name="" id="" />
-        <button className="rounded-md bg-black p-1 text-white dark:bg-white dark:text-black">
+      <form
+        onSubmit={handleSubmit}
+        className="relative flex flex-col gap-4 rounded-lg bg-white  p-6 dark:bg-neutral-900 dark:text-white"
+      >
+        <input
+          type="file"
+          name="img"
+          accept="image/png, image/jpeg"
+          onChange={(e) => setImg(e.target.files[0])}
+        />
+        <div className="min-h-[30px] text-center">
+          <p
+            className={
+              success ? "text-xs text-green-600" : "text-xs text-red-600"
+            }
+          >
+            {success ? success : errors && errors}
+          </p>
+        </div>
+        <button
+          type="submit"
+          className="rounded-md bg-black p-1 text-white dark:bg-white dark:text-black"
+        >
           Edit Picture
         </button>
         <IoMdClose
