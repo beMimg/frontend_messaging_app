9ee1eaa1a49604513a159b409397cca71c40bbad
beMimg <bernardommguerreiro@gmail.com>
2024-03-27 16:14:30 +0100

    Add background svg to message background

--- src/components/Conversation.jsx
+++ src/components/Conversation.jsx
@@ -66,7 +66,7 @@ export default function Conversation({ conversation_id }) {
         <ConversationHeader conversationDetails={conversationDetails} />
       </header>
       <div
-        className="flex-1 overflow-auto bg-white p-4 dark:bg-neutral-900"
+        className="background-message flex-1 overflow-auto p-4 "
         ref={messageContainerRef}
       >
         <Messages
