07b318c30bf113115df03d6feaf766bde8549d46
beMimg <bernardommguerreiro@gmail.com>
2024-03-30 16:40:41 +0100

    RecievedMessage can wrap a really large word

--- src/components/RecievedMessage.jsx
+++ src/components/RecievedMessage.jsx
@@ -19,11 +19,8 @@ export default function RecievedMessage({ conversationDetails, message }) {
           className="h-[45px] w-[45px] rounded-full border border-gray-300  object-cover object-center"
         />
       )}
-      <div
-        className="flex min-w-min max-w-[80%] flex-col gap-1 self-start"
-        key={message._id}
-      >
-        <p className="self-start rounded-r-3xl rounded-tl-3xl bg-black px-4 py-1 text-white dark:bg-white dark:text-black">
+      <div className="flex max-w-[80%] flex-col gap-1 " key={message._id}>
+        <p className="break-words rounded-r-3xl rounded-bl-3xl bg-black px-4 py-1 text-white dark:bg-white dark:text-black">
           {message.content}
         </p>
         <p className="self-start text-xs opacity-35">{formattedDate}</p>
