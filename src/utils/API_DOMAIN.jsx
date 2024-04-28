2eecc2c9baf9fbd9c8cc5c107a6dee3f06b3352e
beMimg <bernardommguerreiro@gmail.com>
2024-04-28 13:33:09 +0200

    Modify API_DOMAIN for production

--- src/utils/API_DOMAIN.jsx
+++ src/utils/API_DOMAIN.jsx
@@ -1,3 +1,2 @@
-// export const API_DOMAIN =
-//   "https://backendmessagingapp-production.up.railway.app/api";
-export const API_DOMAIN = "http://localhost:3000/api";
+export const API_DOMAIN =
+  "https://backendmessagingapp-production.up.railway.app/api";
